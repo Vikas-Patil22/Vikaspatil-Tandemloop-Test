b4ece6f8b59ae39e74566b800433d107e7bebc38
Vikas-Patil22 <vikaspatil11899@gmail.com>
2025-08-07 22:57:21 +0530

    Add files via upload

---
+++ Problem1.java
@@ -0,0 +1,19 @@
+public class Problem1 {
+
+    public static void main(String[] args) {
+        double a = 10;
+        double b = 5;
+        String op = "add"; 
+
+        if (op.equals("add"))
+            System.out.println(a + b);
+        else if (op.equals("subtract"))
+            System.out.println(a - b);
+        else if (op.equals("multiply"))
+            System.out.println(a * b);
+        else if (op.equals("divide"))
+            System.out.println(b != 0 ? a / b : "Cannot divide by zero");
+        else
+            System.out.println("Invalid operation");
+    }
+}
